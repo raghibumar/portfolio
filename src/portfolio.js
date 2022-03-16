b0da46a8f2c746c925368fc14941407bd074917f
ABU RAGHIB UMAR <raghibumar2@gmail.com>
2022-03-17 00:51:24 +0530

    initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,138 @@
+//I Would Highly Recommend You To Customize The Theme According To Your Taste.
+export const globalStyles = {
+
+    "--grad": "linear-gradient(315deg,#48a9fe,#00cdac)", // Use gradients only
+    // "--grad": "linear-gradient(315deg, #48a9fe 0%, #734ae8 74%)", // Use gradients only
+    // "--grad": "linear-gradient(315deg, #06bcfb 0%, #4884ee 74%)",
+    "--txtcolor": "#272341",
+    "--p_color": "#00cdac",
+    "--s_color": "#fff"
+}
+
+// Navigation Bar Name
+export const navbar_name = ""
+
+// Main Page
+export const main = {
+  name: "Abu Raghib Umar",
+  doamin: "Full Stack Web Developer",
+  resume:
+    "https://drive.google.com/file/d/1NAuniVR4SrVYaKxoCang7P4N32EOhJif/view?usp=sharing",
+};
+
+// AboutMe
+export const aboutme = {
+  imgLink: "https://i.imgur.com/gCuk8hO.jpeg",
+  p1: "Hey, My name is Abu Raghib Umar. A Full Stack Developer, with strong administrative and communication skills, good attention to detail and with the ability to write efficient code using MERN Stack.",
+  p2: "I have been developing Web Apps for over 7 months now. Well versed in coding using Javascript and its frameworks.",
+  p3: "As I grow and flourish as a Developer, one thing which keeps me going is my inquisitiveness for discovering new things every day.",
+};
+
+// Skills
+export const whatido = {
+    title: "Full Stack Development",
+
+    details: ["Building responsive website using React",
+        "Creating backend application in Node, Express",
+        "Building Simple RESTful CRUD APIs",
+        "Knowledge of Object-Oriented programming"],
+    logos: [{
+        name: "HTML5",
+        iconifyClassName: "vscode-icons:file-type-html", // PS : Use iconify class name
+        id: "1"
+    },
+    {
+        name: "CSS",
+        iconifyClassName: "vscode-icons:file-type-css",
+        id: "2"
+    },
+    {
+        name: "JavaScript",
+        iconifyClassName: "logos:javascript",
+        id: "3"
+    },
+    {
+        name: "Tailwind",
+        iconifyClassName: "logos:tailwindcss-icon",
+        id: "4"
+    },
+    {
+        name: "NodeJS",
+        iconifyClassName: "logos:nodejs",
+        id: "5"
+    },
+    {
+        name: "MongoDB",
+        iconifyClassName: "logos:mongodb",
+        id: "6"
+    },
+    
+    {
+        name: "ReactJS",
+        iconifyClassName: "vscode-icons:file-type-reactjs",
+        id: "7"
+    },
+    {
+        name: "Redux",
+        iconifyClassName: "logos:redux",
+        id: "8"
+    },
+    
+    {
+        name: "NPM",
+        iconifyClassName: "logos:npm-icon",
+        id: "9"
+    },
+    {
+        name: "VSCode",
+        iconifyClassName: "vscode-icons:file-type-vscode",
+        id: "10"
+    },
+    {
+        name: "PostMan",
+        iconifyClassName: "logos:postman-icon",
+        id: "11"
+    },
+    {
+        name: "Git",
+        iconifyClassName: "bi:git",
+        id: "12"
+    },
+    // {
+    //     name: "Vercel",
+    //     iconifyClassName: "logos:vercel",
+    //     id: "13"
+    // }
+    ]
+}
+
+// Project Details
+export const details = [
+    
+    {
+    id: 1,
+    title: "Clone of Practo",
+    desc: "An informative website that provides online Doctor Consultant, Booking Appointments, Purchasing Medicines, Delivery of Varieties of Medical produts and  many more.  Tech Stack - HTML, CSS, Javascript.",
+    img: "https://pbs.twimg.com/media/FLuT0LzaIAcyZUq?format=jpg&name=900x900",
+    github: "https://github.com/AbhishekKumarJha3098/Practo_clone",
+    link: "https://drive.google.com/file/d/1HVRI-uq7e0DPzpaR9lhRCo9wtMpuT2xV/view?usp=sharing",
+    deploy:"https://karu555.github.io/practoClone/"
+    
+},
+{
+    id: 2,
+    title: "Clone of Purplle",
+    desc: "Purplle is an Indian multi-brand beauty retailer selling cosmetic and wellness products. It is an E-commerce site where users can buy all cosmetics related products . Tech Stack - HTML, CSS, Javascript, ExpressJS, Mongodb, NodeJS.",
+    img: "https://pbs.twimg.com/media/FL4LmdGaAAAUoOA?format=jpg&name=900x900",
+    link: "",
+    github: "https://github.com/Karu555/Purplle-Clone",
+    deploy:"https://festive-shirley-d33e4c.netlify.app/"
+}
+
+]
+
+// Social Media Links
+export const links = ["https://www.linkedin.com/in/karunesh-ranjan-6515211a0",
+    "https://github.com/Karu555",
+    "https://www.instagram.com/karunesh_ranjan/",
+    "https://twitter.com/karuneshranjan1"]
