725e35f4f4cae4661877884e864051fb0754a87f
ABU RAGHIB UMAR <raghibumar2@gmail.com>
2022-03-17 04:03:10 +0530

    updated contact details

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,19 +5,33 @@ import Fade from 'react-reveal/Fade';
 import "./contact.css"
 
 const Contact = () => {
-    return <div className="contact" id="contact">
+    return (
+      <div className="contact" id="contact">
         <h1 className="contact-title">
-            <Fade bottom cascade duration={2500}>Reach Out To Me!</Fade>
+          <Fade bottom cascade duration={2500}>
+            Reach Out To Me!
+          </Fade>
         </h1>
         <div className="icon">
-            {links.map((link) => <div className="icons"> <SocialIcon url={link} target="_blank"
- /> </div>)}
+          {links.map((link) => (
+            <div className="icons">
+              {" "}
+              <SocialIcon url={link} target="_blank" />{" "}
+            </div>
+          ))}
         </div>
         <center>
-            <hr className="style-f" />
+          <hr className="style-f" />
         </center>
-        <p className="copyright">Socializing and Connecting is really important, which leads to great ideas and major breakthroughs. <br />You can contact me at the places mentioned above or You can call me at +91 7979761092</p>
-    </div >
+        <p className="copyright">
+          Socializing and Connecting is really important, which leads to great
+          ideas and major breakthroughs. <br />
+          You can contact me at the places mentioned above or You can connect me
+          at <b>raghibumar2@gmail.com</b> or You can call me at{" "}
+          <b>+91 7542904942</b>
+        </p>
+      </div>
+    );
 }
 
 export default Contact
